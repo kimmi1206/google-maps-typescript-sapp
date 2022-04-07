aaab2754f9e239b7b2c01c92a4a27b40632008e6
kimmi1206 <kheinerimm1206@gmail.com>
2022-04-07 07:17:53 -0500

    simple google maps typescript app
    simple google maps typescript app using google maps javascript api

---
+++ src/User.ts
@@ -0,0 +1,24 @@
+import { faker } from '@faker-js/faker';
+import { Mappable } from './CustomMap';
+
+export class User implements Mappable {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  constructor() {
+    this.name = faker.name.findName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+  markerContent(): string {
+    return `
+      <div>
+        <h2>User: ${this.name}</h2>
+      </div>
+    `;
+  }
+}
